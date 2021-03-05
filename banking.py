5ce4620c5befb64413cf7199d2549bab08306178
UnderAnder <andryushin.alexander@gmail.com>
2021-03-06 02:49:31 +0700

    init, cardgen, basic menu

---
+++ banking.py
@@ -0,0 +1,103 @@
+import random
+
+
+class Account:
+    def __init__(self, number, pin):
+        self.card = None
+        if number is None and pin is None:
+            self.new_account()
+        else:
+            self.login(number, pin)
+
+    def new_account(self):
+        self.card = Card()
+        print('Your card has been created')
+        print('Your card number:')
+        print(self.card.number)
+        print('Your card PIN:')
+        print(Card.card_database[self.card.number])
+
+    def login(self, number, pin):
+        card_database = Card.card_database
+        if number in card_database:
+            if pin == card_database[number]:
+                print('You have successfully logged in!')
+                Menu('account_menu')
+        else:
+            print('Wrong card number or PIN!')
+
+class Card:
+    card_database = {}
+
+    def __init__(self):
+        self.number = self.generate_card()
+
+    def generate_card(self):
+        card_number_bin = '400000'
+        card_number_checksum = '8'
+        card_number_ain = random.randint(100000000, 999999999)
+        card_number = card_number_bin + str(card_number_ain) + card_number_checksum
+
+        if card_number in self.card_database:
+            self.generate_card()
+        card_pin = random.randint(1000, 9999)
+        self.card_database[card_number] = card_pin
+        return card_number
+
+
+class Menu:
+    def __init__(self, state):
+        self.state = state
+        if state == 'main_menu':
+            self.main_menu()
+        if state == 'account_menu':
+            self.account_menu()
+
+    def main_menu(self):
+        print('1. Create an account')
+        print('2. Log into account')
+        print('0. Exit')
+
+        self.choice(input(), self.state)
+
+    def login_menu(self):
+        card_number = input('Enter your card number:\n')
+        card_pin = input('Enter your PIN:\n')
+        account = Account(card_number, card_pin)
+
+    def account_menu(self):
+        print('1. Balance')
+        print('2. Log out')
+        print('0. Exit')
+
+    def choice(self, n, state):
+        if state == 'main_menu':
+            if n == '1':
+                self.create_account()
+            elif n == '2':
+                self.login_account()
+            elif n == '0':
+                self.exit()
+            else:
+                print('input 1, 2 or 0 for exit')
+                self.choice(input(), self.state)
+        elif state == 'login_menu':
+            pass
+
+    def create_account(self):
+        account = Account(None, None)
+
+    def login_account(self):
+        pass
+
+    def exit(self):
+        print('Bye!')
+        return
+
+
+def main():
+    menu = Menu('main_menu')
+
+
+if __name__ == '__main__':
+    main()
