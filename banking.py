c92a01bcadeefd9edff3a904440e616f07e13637
Under Ander <andryushin.alexander@gmail.com>
2021-03-06 12:13:16 +0700

    account

--- banking.py
+++ banking.py
@@ -3,35 +3,31 @@ import random
 
 class Account:
     def __init__(self, number, pin):
-        self.card = None
+        self.balance = 0
         if number is None and pin is None:
-            self.new_account()
+            self.card = self.new_account()
         else:
-            self.login(number, pin)
+            self.card = Card().auth(number, pin)
 
     def new_account(self):
-        self.card = Card()
+        card = Card().generate_card()
         print('Your card has been created')
         print('Your card number:')
-        print(self.card.number)
+        print(card.number)
         print('Your card PIN:')
-        print(Card.card_database[self.card.number])
+        print(card.pin)
+        return card
 
-    def login(self, number, pin):
-        card_database = Card.card_database
-        if number in card_database:
-            if pin == card_database[number]:
-                print('You have successfully logged in!')
-                Menu('account_menu')
-        else:
-            print('Wrong card number or PIN!')
+    def show_balance(self):
+        print('Balance:', self.balance)
 
 
 class Card:
     card_database = {}
 
     def __init__(self):
-        self.number = self.generate_card()
+        self.number = None
+        self.pin = None
 
     def generate_card(self):
         card_number_bin = '400000'
@@ -43,12 +39,25 @@ class Card:
             self.generate_card()
         card_pin = random.randint(1000, 9999)
         self.card_database[card_number] = card_pin
-        return card_number
+        self.number = card_number
+        self.pin = card_pin
+        return self
+
+    def auth(self, number, pin):
+        if number in self.card_database:
+            if pin == self.card_database[number]:
+                print('You have successfully logged in!')
+                Menu('account_menu')
+                return self
+        else:
+            print('Wrong card number or PIN!')
+        return None
 
 
 class Menu:
     def __init__(self, state):
         self.state = state
+        self.account = None
         if state == 'main_menu':
             self.main_menu()
         if state == 'account_menu':
@@ -59,7 +68,7 @@ class Menu:
         print('2. Log into account')
         print('0. Exit')
 
-        self.choice(input(), self.state)
+        self.choice(input(), 'main_menu')
 
     def login_menu(self):
         card_number = input('Enter your card number:\n')
@@ -71,23 +80,33 @@ class Menu:
         print('2. Log out')
         print('0. Exit')
 
+        self.choice(input(), 'account_menu')
+
     def choice(self, n, state):
         if state == 'main_menu':
             if n == '1':
-                Account(None, None)
+                self.account = Account(None, None)
             elif n == '2':
                 self.login_menu()
             elif n == '0':
                 self.exit()
             else:
                 print('input 1, 2 or 0 for exit')
-                self.choice(input(), self.state)
-        elif state == 'login_menu':
-            self.account_menu()
+                self.choice(input(), 'main_menu')
+        elif state == 'account_menu':
+            if n == '1':
+                self.account.show_balance()
+            elif n == '2':
+                self.login_menu()
+            elif n == '0':
+                self.exit()
+            else:
+                print('input 1, 2 or 0 for exit')
+                self.choice(input(), 'account_menu')
 
     def exit(self):
         print('Bye!')
-        return
+        exit()
 
 
 def main():
